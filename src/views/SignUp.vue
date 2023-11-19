da62f3c55d9c1dc989ac7bd8f34e5042cc1f6ed6
mammadli <gulnar.mammadli@ut.ee>
2023-11-19 14:13:17 +0200

    Update validatePassword logic

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -42,14 +42,16 @@ export default {
         validateForm() {
             let regex = /^[A-Z](?=.*[0-9])(?=.*_)(?=.*[a-z].*[a-z])[A-Za-z0-9_]*$/;
             let second_condition = regex.test(this.password);
+            isLengthValid = this.password.length < 8 || this.password.length > 15;
 
-            if (this.password.length < 8 || this.password.length > 15) {
+            if (!isLengthValid) {
                 console.log(this.password.length);
                 this.validatePassword = "Password must be between 8-15 chars";
             } else if (!second_condition) {
-                console.log(second_condition);
                 this.validatePassword =
                     "The password is not valid! It must contain a combination of the first letter being Uppercase,at 2 Lowercase characters, 1 Digits (0-9), and _";
+            } else {
+                this.validatePassword = "";
             }
         },
     },
